3f95ac7d923662eb921d009391d9ef3c69579f4d
gannicottb <brandon.gannicott@gmail.com>
2019-10-15 11:36:21 -0500

    Create exercises.scala

---
+++ exercises.scala
@@ -0,0 +1,27 @@
+object ExercisesModule {
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, a: Int, b: Int): Int = {
+      if (n <= 0) a
+      else go(n - 1, b, a + b)
+    }
+    go(n, 0, 1)
+  }
+
+  def isSorted[A] (as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    if (as.length < 2) return true // sorted by definition
+    def go(index: Int, sorted: Boolean): Boolean = {
+      if (sorted == false || index >= as.length) sorted
+      else go(index + 1, ordered(as(index), as(index + 1)))
+    }
+    go(0, sorted = true)
+  }
+  def main(args: Array[String]): Unit = {
+    // 2.1
+    0.to(end = 10) foreach (i => println("%s: %s".format(i, fib(i))))
+
+    // 2.2
+    val a: Array[Int] = Array(1, 3, 2)
+    println(isSorted(a, (x, y) => x < y : Boolean))
+  }
+}
